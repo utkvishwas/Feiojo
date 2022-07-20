7b8604602de0c1948adf6f226613c7670e1aa2ac
Utkarsh Vishwas <utkarshvishwas@gmail.com>
2022-07-20 15:48:58 +0530

    updated

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(addRoutes);
 app.use(updateRoutes);
 app.use(deleteRoutes);
 
-const port = proces.env.PORT || 3001;
+const port = 3001;
 
 app.listen(port, function () {
   console.log("server has started at port " + port);
